c285f4ce07308fcf72d035f960738276ca9115fd
Damien Pierre <>
2024-07-24 17:00:33 +0200

    base vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @type {import('vite').UserConfig} */
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteSingleFile()],
+  assetsInclude: ["public/*.svg"],
+  build: {
+    emptyOutDir: false,
+    cssCodeSplit: false,
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: "assets/[name]-[hash][extname]",
+    //     chunkFileNames: "assets/[name]-[hash].js",
+    //     entryFileNames: "assets/[name]-[hash].js",
+    //   },
+    // },
+  },
+} satisfies UserConfig);
